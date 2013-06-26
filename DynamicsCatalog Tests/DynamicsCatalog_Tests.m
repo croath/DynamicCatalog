a93db04ed1d11bf8da124780c81ddc198aeaee58
loulianpi <mijuni7@gmail.com>
2013-06-26 14:21:12 +0800

    add XCUnit TestCase

---
+++ DynamicsCatalog Tests/DynamicsCatalog_Tests.m
@@ -0,0 +1,41 @@
+//
+//  DynamicsCatalog_Tests.m
+//  DynamicsCatalog Tests
+//
+//  Created by 芈峮 on 13-6-26.
+//  Copyright (c) 2013年 Apple Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DynamicsCatalog_Tests : XCTestCase
+
+@end
+
+@implementation DynamicsCatalog_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  XCTAssertTrue(NO, @"some error here,and must be fixed!");
+}
+
+- (void)testSeccussed
+{
+  XCTAssertTrue(YES);
+}
+
+@end
